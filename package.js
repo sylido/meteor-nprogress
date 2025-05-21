bccaaa54123c563145555f5bfa2f8f35f43faeb6
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-05-21 17:45:43 -0600

    Updated to make it work with Meteor 3.x
    Deleted versions and .travis.yml files
    
    Updated dependencies in package.js file for Meteor 3.2.2, updated nprogress package version to 0.2.1 from 0.2.0
    Updated dependenciese in versions.json file
    
    Reformatted smart.json not sure if still needed
    Updated test-nprogress.js - fixed typos, reformatted
    
    Pulled in the nprogress submodule, included both nprogress.js/.css and the license.md file to be used locally

--- package.js
+++ package.js
@@ -1,19 +1,21 @@
 Package.describe({
-    summary: "NProgress for Meteor",
-    version: "0.2.0",
-    git: "https://github.com/zhouzhuojie/meteor-nprogress.git",
-    name: "mrt:nprogress"
+  summary : "NProgress for Meteor",
+  version : "0.2.1",
+  git     : "https://github.com/sylido/meteor-nprogress.git",
+  name    : "sylido:nprogress"
 });
 
 Package.onUse(function (api) {
-    api.use('jquery@1.0.0', 'client');
-    api.add_files('lib/nprogress/nprogress.js', 'client');
-    api.add_files('lib/nprogress/nprogress.css', 'client');
-    api.add_files('lib/main.js', 'client');
-    api.export('NProgress', 'client');
+  api.use("jquery", "client"); // removed jquery version restriction
+
+  api.add_files("lib/nprogress/nprogress.js", "client");
+  api.add_files("lib/nprogress/nprogress.css", "client");
+  api.add_files("lib/main.js", "client");
+
+  api.export("NProgress", "client");
 });
 
 Package.onTest(function (api) {
-    api.use(['mrt:nprogress', 'tinytest'], 'client');
-    api.add_files('test-mrt:nprogress.js', 'client');
+  api.use(["sylido:nprogress", "tinytest"], "client");
+  api.add_files("test-nprogress.js", "client");
 });
--- test-nprogress.js
+++ test-nprogress.js
@@ -1,6 +1,6 @@
-//Tests
+// Tests
 Tinytest.add('NProgress can be initalized', function (test) {
-  test.isNotNull(NProgress, 'nprogres should exist');
-  test.isTrue(typeof(NProgress) === "object", 'nprogres should be an object');
-  test.isTrue(typeof(NProgress.start) === "function", 'nprogres should be a function');
+  test.isNotNull(NProgress, 'nprogress should exist');
+  test.isTrue(typeof(NProgress) === "object", 'nprogress should be an object');
+  test.isTrue(typeof(NProgress.start) === "function", 'nprogress should be a function');
 });
